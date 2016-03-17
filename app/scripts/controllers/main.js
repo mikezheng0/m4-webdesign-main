068b20bb75901f9a0ef81df655a0ffc9210a0b7a
Matt Lee <matt_lee07@live.ca>
2016-03-17 17:18:25 -0400

    Added scope to controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,8 @@
  * Controller of the m4WebDevApp
  */
 angular.module('m4WebDevApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
