de9aae05a6dab8370e4f124939f2a1d63d65a88f
Mike Zheng <mike.zheng@mohawkcollege.ca>
2016-03-21 19:11:52 -0400

    saved scroll spy to bower json

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,21 @@
  */
 angular.module('m4WebDevApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+  })
+  .controller('myCtrl', function($scope, $document) {
+    var top = 400;
+    var duration = 2000; //milliseconds
+
+    //Scroll to the exact position
+    $document.scrollTop(top, duration).then(function() {
+      console && console.log('You just scrolled to the top!');
+    });
+
+    var offset = 30; //pixels; adjust for floating menu, context etc
+    //Scroll to #some-id with 30 px "padding"
+    //Note: Use this in a directive, not with document.getElementById
+    var someElement = angular.element(document.getElementById('some-id'));
+    $document.scrollToElement(someElement, offset, duration);
+  }
+);
