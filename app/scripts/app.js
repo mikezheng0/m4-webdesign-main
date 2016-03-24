c3fbcb1e16628aa81d57c7c4dde38bc1587f3dba
Mike <mike@Mikes-iMac.asus.router.com>
2016-03-23 21:18:09 -0400

    added banner directive

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,7 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch',
+    'ngMaterial',
     'duScroll'
   ])
   .config(function ($routeProvider) {
@@ -33,4 +33,9 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .config(function($mdIconProvider) {
+    $mdIconProvider
+      .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
+      .defaultIconSet('img/icons/sets/core-icons.svg', 24);
   });
